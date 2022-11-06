e6eb9635f36c1391c60c28f008960f6ff9bf384e
Dlite <afekhide009@gmail.com>
2022-11-07 00:48:44 +0100

    Added storage support

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 import {Server} from 'socket.io';
 import {createServer} from 'http';
+import { config } from 'dotenv';
+import {createClient} from '@supabase/supabase-js';
 
+config();
 const PORT = process.env.PORT || 4000;
 let clients = []
+const supabase = createClient(process.env.supabase_url, process.env.supabase_key);
 
 const httpServer = createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('Welcome to Simple Chat Server')
     res.end()
 })
+
 const server = new Server(httpServer, {
     cors: {
         origin: "*"
@@ -17,9 +22,15 @@ const server = new Server(httpServer, {
 
 server.on('connection', socket => {
     console.log(`${socket.id} connected...`);
+    socket.emit('fetchMessages',{db_url: process.env.supabase_url, db_key: process.env.supabase_key});
 
     socket.on('message', data => {
-        server.emit('message', data)
+        server.emit('message', data);
+        let {text} = JSON.parse(data);
+        (async function(){
+            const {error} = await supabase.from('message').insert({text});
+            error? console.log(error) : console.log('message sent successfully');
+        })()
     })
 
     socket.on('register', data => {
@@ -27,13 +38,11 @@ server.on('connection', socket => {
             id: socket.id,
             username: JSON.parse(data).username
         })
-        console.log(clients)
     })
 
     socket.on('disconnect', reason => {
         console.log(`${socket.id} disconnected...`)
         clients = clients.filter(client => client.id !== socket.id)
-        //console.log(clients)
     })
     
 })
